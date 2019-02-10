b0650b17887612c7c146b7b3122203eecda51e6d
sun12002 <sufian.nazim@uconn.edu>
2019-02-10 09:51:07 -0500

    added broken junit to test status check

--- src/test/java/com/jenkins/integration/demo/DemoApplicationTests.java
+++ src/test/java/com/jenkins/integration/demo/DemoApplicationTests.java
@@ -1,17 +1,24 @@
 package com.jenkins.integration.demo;
 
+import com.jenkins.integration.demo.controller.AdditionController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	private AdditionController additionController;
 
+	@Test
+	public void testAdditionController() {
+		assertEquals(new Integer(19), additionController.addTwoNumbers(10, 10));
+	}
 }
 
